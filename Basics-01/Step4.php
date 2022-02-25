4f9b99ea1977159eff599b37d45cac7f9020fc82
SouadTorfi <souad.torfi99@gmail.com>
2022-02-25 16:35:12 +0200

    Step4.php

---
+++ Basics-01/Step4.php
@@ -0,0 +1,9 @@
+<?php
+
+$name='souad';
+$position='tech consultant';
+$github_url='https://github.com/SouadTorfi';
+echo "Hello, I'm $name ,"."I'm a $position,"."please check my github link $github_url \n";
+echo "Hello, I'm $name ,I'm a $position,please check my github link $github_url \n";
+echo 'Hello, I\'m ' .$name .',I\'m a '.$position. ',please check my github link '.$github_url;
+?>
