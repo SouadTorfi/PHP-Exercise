68ab8616834dbd4b0d48045505b0ba2a4d49af38
SouadTorfi <souad.torfi99@gmail.com>
2022-02-26 12:31:06 +0200

    Step8_a.php

---
+++ Basics-01/Step8_a.php
@@ -0,0 +1,5 @@
+<?php
+
+$list = array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon"); 
+echo "Hello Sir, do you have ".$list[0].",".$list[1].",".$list[2]."? Also if you sell fruits can I find a ".$list[5]."? \n";
+?>
