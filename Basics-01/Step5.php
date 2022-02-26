a318925cd645702ff0d3f9695cd5abcef7f78414
SouadTorfi <souad.torfi99@gmail.com>
2022-02-26 11:16:11 +0200

    Step5.php

---
+++ Basics-01/Step5.php
@@ -0,0 +1,10 @@
+
+<?php
+$course_name='Rust, the language of no bugs';
+$enrolled_students=20;
+$price =3.5;
+$on_discount=false;
+
+
+echo " Course title:".$course_name."\n Enrolled Students:".$enrolled_students."\n Course price:".$price."$ USD" ."\n Course on discount:" .($on_discount ? 'yes' : 'no'). "\n";
+?>
