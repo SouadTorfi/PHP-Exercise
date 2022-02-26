025f9108bf43acd148956660b773a07b88a13339
SouadTorfi <souad.torfi99@gmail.com>
2022-02-26 13:28:21 +0200

    Step8_b.php

---
+++ Basics-01/Step8_b.php
@@ -0,0 +1,22 @@
+<?php
+$marks = array(
+    "eggs" => array (
+        0 => "balade",
+        1 => "mazare3"
+     ),
+     "milk" => array (
+        0 => "Fresh",
+        1 => "Taanayel",
+     ),
+     "water-pack" => array (
+        0 => "Tannoureen",
+        1 => "Reem",
+        )
+  );
+  
+echo "Hey Sir, Please I need 1 pack of ".$marks['eggs'][0]." eggs and 3 ".$marks['water-pack'][1]." Water Pack \n"; 
+
+      
+?>
+
+?>
