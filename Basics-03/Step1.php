d0b2ae15dadeaa8a67d7fd093337421412b2237c
SouadTorfi <souad.torfi99@gmail.com>
2022-02-28 12:07:44 +0200

    edit Step1.php

--- Basics-03/Step1.php
+++ Basics-03/Step1.php
@@ -1,10 +1,24 @@
 <?php
-for($row=1;$row<=8;$row++)
-{
-   for ($star=1;$star<=$row;$star++)
-    {
-     echo "*";
-     }
-     echo "\n";
+// for($row=1;$row<=8;$row++)
+// {
+//    for ($star=1;$star<=$row;$star++)
+//     {
+//      echo "*";
+//      }
+//      echo "\n";
+// }
+
+function stars($n){
+    $x = 0;
+    while ($x < $n) {
+        for ($i=0; $i <= $x ; $i++) { 
+            echo "*";
+        };
+        echo "\n";
+        $x ++;
+    }
 }
+
+stars(8)
+
 ?>
